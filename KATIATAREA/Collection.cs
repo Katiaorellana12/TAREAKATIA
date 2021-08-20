cb3a8025da0c6cadb7d4b95d9f097bb94ab6be92
Katiaorellana12 <88348464+Katiaorellana12@users.noreply.github.com>
2021-08-19 22:10:31 -0600

    Add files via upload

---
+++ KATIATAREA/Collection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KATIATAREA
+{
+    public class Collection
+    {
+        public static void SortedListCollection()
+        {
+            //factorial de un numero n
+            int n, cont, fact;
+            Console.Write("Digite el valor de n! : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine("El factorial de {0} es 1", n);
+            }
+            else
+            {
+                fact = n;
+                for (cont = n; cont > 1; cont--)
+                {
+                    fact = fact * (cont - 1);
+                }
+                Console.WriteLine("El factorial de {0} es: {1} ", n, fact);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
